8cdbfe8d57015586500a49d1f90223bc70dc8c69
Onur GOKCE <onurgokcecom@gmail.com>
2023-01-08 19:34:31 +0300

    some thing

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,12 @@ var bodyParser = require('body-parser')
 
 var jsonParser = bodyParser.json()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -15,8 +21,6 @@ app.post('/translator', jsonParser, async (req, res) => {
     console.log(body)
     const translation = await translator.translator(body.text)
     console.log('translation: ', translation)
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.json({translation: translation})
 })
 
