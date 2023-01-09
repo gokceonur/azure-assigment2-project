e5517bffce8704fcbc9cd30dd3575eaec4a3aa1d
Onur GOKCE <onurgokcecom@gmail.com>
2023-01-09 05:27:03 +0300

    some changes have been made

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-function translateText() {
+const translateText = () => {
     const inputText = document.getElementById("input-text").value;
     var requestBody = [{
       "text": inputText
@@ -26,47 +26,47 @@ function translateText() {
         const json = JSON.parse(result)
         console.log(result)
         document.getElementById("output-text").innerHTML = json.translation
+        
+        const history = document.getElementById("history");
+        const div = document.createElement("div");
+        div.innerHTML = inputText + " -> " + json.translation;
+        history.insertBefore(div, history.firstChild);
+
       })
       .catch(error => console.log('error', error));
     
 }
 
-const dummy = [
-    {
-        "text": "Hello World",
-        "translation": "Merhaba Dunya",
-        "creation_date": "2020-12-01T12:00:00Z",
-        "id": 1
-    },
-    {
-        "text": "Hello World",
-        "translation": "Merhaba Dunya",
-        "creation_date": "2020-12-01T12:00:00Z",
-        "id": 2
-    },
-    {
-        "text": "Hello World",
-        "translation": "Merhaba Dunya",
-        "creation_date": "2020-12-01T12:00:00Z",
-        "id": 3
-    },
-    {
-        "text": "Hello World",
-        "translation": "Merhaba Dunya",
-        "creation_date": "2020-12-01T12:00:00Z",
-        "id": 4
-    },
-]
+const getHistory = () => {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Access-Control-Allow-Origin", "*");
+  myHeaders.append("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
 
-const printHistory = () => {
-    const history = document.getElementById("history");
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
 
-    dummy.forEach((item) => {
-        const div = document.createElement("div");
-        div.innerHTML = item.text + " -> " + item.translation;
-        history.appendChild(div);
+  fetch("http://translator.westeurope.cloudapp.azure.com:80/history/", requestOptions)
+    .then(response => response.text())
+    .then((result) => {
+      const json = JSON.parse(result)
+      
+      const history = document.getElementById("history");
+
+      json.history.forEach((item) => {
+          const div = document.createElement("div");
+          div.innerHTML = item.date + "  " + item.input + " -> " + item.translation;
+          history.append(div);
+      })
+
+      console.log(json)
+      return json;
     })
-
+    .catch(error => console.log('error', error));
+  
 }
 
-printHistory();
+window.onload = () => getHistory();
